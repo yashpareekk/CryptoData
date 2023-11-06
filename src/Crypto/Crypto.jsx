4d5748fa3a5af0113b0f911cb8bb1c0ceca96c7d
GodfatherYash <pareekyash10@gmail.com>
2023-11-06 11:52:08 +0530

    Title

--- src/Crypto/Crypto.jsx
+++ src/Crypto/Crypto.jsx
@@ -74,3 +74,78 @@ const Crypto=() => {
 }
  
 export default Crypto;
+
+
+
+
+/*
+import "./Crypto.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+const Crypto = () => {
+    const [search, setSearch] = useState("");
+    const [crypto, setCrypto] = useState([]);
+    const [filteredCrypto, setFilteredCrypto] = useState([]);
+
+    useEffect(() => {
+        Axios.get(
+            `https://api.coinstats.app/public/v1/coins?skip=0&limit=100&currency=INR`
+        ).then((res) => {
+            setCrypto(res.data.coins);
+        });
+    }, []);
+
+    useEffect(() => {
+        setFilteredCrypto(
+            crypto.filter((val) =>
+                val.name.toLowerCase().includes(search.toLowerCase())
+            )
+        );
+    }, [search, crypto]);
+
+    const tableHeaders = ['Rank', 'Name', 'Symbol', 'Market Cap', 'Price', 'Available Supply', 'Volume(24hrs)'];
+
+    return (
+        <div className="App">
+            <h1>All Cryptocurrencies</h1>
+            <input
+                type="text"
+                placeholder="Search..."
+                onChange={(e) => {
+                    setSearch(e.target.value);
+                }}
+            />
+            <table>
+                <thead>
+                    <tr>
+                        {tableHeaders.map((header, index) => (
+                            <td key={index}>{header}</td>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredCrypto.map((val, id) => (
+                        <tr key={id}>
+                            <td className="rank">{val.rank}</td>
+                            <td className="logo">
+                                <a href={val.websiteUrl}>
+                                    <img src={val.icon} alt="logo" width="30px" />
+                                </a>
+                                <p>{val.name}</p>
+                            </td>
+                            <td className="symbol">{val.symbol}</td>
+                            <td>₹{val.marketCap}</td>
+                            <td>₹{val.price.toFixed(2)}</td>
+                            <td>{val.availableSupply}</td>
+                            <td>{val.volume.toFixed(0)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Crypto;
+ */
